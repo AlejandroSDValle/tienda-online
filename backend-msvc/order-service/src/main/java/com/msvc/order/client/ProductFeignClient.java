42767d90081e188a74ff2715697c61e94d5c316c
AlejandroSDValle <alejandro_santillan_2000@hotmail.com>
2025-07-30 14:23:16 -0600

    remove ;

--- backend-msvc/order-service/src/main/java/com/msvc/order/client/ProductFeignClient.java
+++ backend-msvc/order-service/src/main/java/com/msvc/order/client/ProductFeignClient.java
@@ -1,7 +1,7 @@
 package com.msvc.order.client;
 
 import com.msvc.order.dto.product.ProductResponse;
-import org.springframework.cloud.openfeign.FeignClient;;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
